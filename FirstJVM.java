4c2074ade81fd92a7b97d0f428576165da6fef1e
Aishwarya Dixit <aishwarya.dixit@sitpune.edu.in>
2017-05-31 11:31:52 +0530

    First commit
    Helper class for process builder example.

---
+++ FirstJVM.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+
+/**
+ * Created by aishwarya on 29/5/17.
+ */
+public class FirstJVM {
+    public  static void  main (String args []) throws IOException, InterruptedException {
+        System.out.println("First Jvm");
+
+
+
+//        ProcessHelper p = new ProcessHelper();
+//        String s [] = new  String[1];
+//        s[0] = "/home/aishwarya/dead.letter";
+//        Thread.sleep(4000);
+//        p.startNewJavaProcess("gedit",FirstJVM.class.getName(),s);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        SecondJVM secondJVM = new SecondJVM();
+//
+//            try {
+//                secondJVM.startSecondJVM();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+
+
+//        try {
+//            Process p = new ProcessBuilder("javac", "apex/apex-malhar-master/demos/PracticeProgs/").start();
+//            System.out.println(p.getErrorStream());
+//           // Process p2= new ProcessBuilder("cd", "apex/apex-malhar-master/demos/PracticeProgs/").start();
+//           // Process p3 = new ProcessBuilder("javac", "ArrayPairSum.java").start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+       System.exit(0);
+    }
+}
